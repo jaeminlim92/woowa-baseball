88e3017fd5cfe6e194d020005d64fd9868c3b4ba
jaeminlim <jaeminlim@lguplus.co.kr>
2023-03-02 21:02:40 +0900

    게임 개발 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,5 @@ import baseball.game.Baseball;
 public class Application {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
-        baseball.startGame();
     }
 }
